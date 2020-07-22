9aefff3bebe3595554e15db5d5a8863fb0e75bc4
jonathan warner <j.m.warners@gmail.com>
2020-07-22 16:32:27 -0700

    MVP and Stretch complete

--- new-user/src/Form.js
+++ new-user/src/Form.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Button } from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from 'reactstrap';
 
 export default function Form (props) {
     const { 
